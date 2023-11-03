fe53e161103d80be18de1559590c7c34ca531000
Hannele Kormano <hkormano@pagerduty.com>
2023-11-03 18:48:57 -0400

    more thoughts

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # editor
 Not just a linter, an editor to help you write human-readable code.
+
+Some folks are going to hate it. That's ok! I don't think this is ever going to be a tool for everyone, and I don't think I'd ever encourage having it as a blocking step in a build pipeline.
+
+It's purely a tool to help make suggestions about how your code could be made to be more readable by other humans.
--- editor.py
+++ editor.py
@@ -3,3 +3,20 @@
   # run editor
   # collect results
   # output results
+
+# editor
+# - general "density"
+# - max line length
+# - number of lines in a block (before space or other separator)
+# - number of lines in a file
+# - number of files in a directory
+# - max indent level
+# - max number of methods in a file
+# - max directory depth
+# - max variable name length
+# - max repetition of non-dictionary words (avoid acronyms)
+# - overly repetitive terms (not keywords of course)
+# - comment to code ratio (rather than needing to document each block, look at the overall ratio of comment lines to code lines)
+
+# language specific
+# - daisy-chain methods vs controller method

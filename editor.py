5d9736f1707d567a4a85b6b26d0ccc7db06e7ac1
Hannele Kormano <hkormano@pagerduty.com>
2023-11-03 18:49:38 -0400

    special chars

--- editor.py
+++ editor.py
@@ -15,6 +15,7 @@
 # - max directory depth
 # - max variable name length
 # - max repetition of non-dictionary words (avoid acronyms)
+# - max density of special characters on a line
 # - overly repetitive terms (not keywords of course)
 # - comment to code ratio (rather than needing to document each block, look at the overall ratio of comment lines to code lines)
 

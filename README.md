63fcd7f56adc92a1a58df78d369c81e7ff157997
Hannele Kormano <hkormano@pagerduty.com>
2023-11-06 11:39:23 -0500

    more notes

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # editor
 I swore I made this private but I guess I didn't :laughing: There is nothing here to see yet really, just notes on what this could be.
 
-Not just a linter, an editor to help you write human-readable code.
+Not just a linter, an editor to help you write scalable, human-readable code. (Scalable here meaning: to any size of codebase, not to the amount of traffic.)
+
+Because sometimes, when people complain about a framework and its scalability, they are actually complaining about how hard it is to find anything.
 
 Some folks are going to hate the idea. That's ok! I don't think this is ever going to be a tool for everyone, and I don't think I'd ever encourage, say, having it as a blocking step in a build pipeline.
 
 It's also not intended to supplant language-specific linters -- if this tool contradicts any of those more specific tools, please accept their recommendations instead!
 
-It's purely to help make suggestions about code readability! And all such automated suggestions should only be applied with your best judgement--at the end of the day, you're the human, and you'll always be the best entity to make stylistic judgement calls about your codebase.
+It's purely to help make suggestions about code navigatability and readability! And all such automated suggestions should only be applied with your best judgement -- at the end of the day, you're the human, and you'll always be the best entity to make stylistic judgement calls about your codebase.
--- editor.py
+++ editor.py
@@ -1,28 +1,36 @@
-# get list of files
-# for file in file list
-  # run editor
+### directory-level editor
+# count number of sub-directory
+# count number of files
+# collect results
+# for file in files
+  # run file-level editor
   # collect results
-  # output results
+# for directory in sub-directories
+  # run directory-level editor
+# output results
 
-# editor
-# - general "density"
-# - max line length
+### file-level editor
+# language agnostic (although configurable) -- general "density"
+# - line length
 # - number of lines in a block (empty line or other separator)
 # - number of lines in a file
 # - number of files in a directory
-# - max indent level
-# - max number of blocks in a file
-# - max directory depth
-# - max variable name length
-# - max repetition of non-dictionary words (avoid acronyms)
-# - max density of special characters on a line
+# - number of directories in a directory
+# - indent level
+# - number of blocks in a file
+# - directory depth
+# - variable name length
+# - repetition of non-dictionary words (avoid acronyms)
+# - density of special characters on a line
 # - overly repetitive terms (not keywords of course)
 # - comment to code ratio (rather than needing to document each block, look at the overall ratio of comment lines to code lines)
 
 # more language specific
+# - ignore library code of course!
 # - method call tracing
-  # - max call stack depth
-  # - max scroll diff (how often are you going up and down, back and forth)
-  # - daisy-chain methods vs controller method
+  # - max non-recursive call stack depth
+  # - daisy-chain methods vs controller method (a->b->c->d vs a->b a->c a->d)
+  # - max scroll diff (how far are you going up and down, how often do you have to switch directions)
+  # - max file changes in stack trace
 # - avoid use of `not` in complex expressions
 # - avoid mixing `and` and `or`
